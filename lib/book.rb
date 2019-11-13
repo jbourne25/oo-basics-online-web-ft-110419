655f80c62020b7c550b57566193bebdb27805769
Jeremy Bourne <jbourne25@yahoo.com>
2019-11-13 19:58:27 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -5,7 +5,25 @@ class Book
   end
   
   def title
+    @title 
   end 
+  
+  def author=(author)
+    @author = author 
+  end 
+ 
+  def author
+    @author
+  end 
+  
+  def page_count=(num)
+    @page_count = num 
+  end 
+  
+  def page_count
+    @page_count
+  end 
+  
   
 end 
   
